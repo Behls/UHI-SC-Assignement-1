8f4380bc46d508a9ec4340634331d0f8324d0852
Behls <alannazimbehl@gmail.com>
2022-07-24 15:48:37 +0100

    adding node class

---
+++ src/queuemanager/Node.java
@@ -0,0 +1,45 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package queuemanager;
+
+/**
+ * @author Alanna Zimbehl - 19016467
+ */
+public class Node {
+    
+    /**
+ contains the person and the priority fields/data for person object within the node
+ */
+    private PriorityItem data;
+    
+//    next field for bridge of next node
+    private Node next;
+    
+     public Node(PriorityItem data){
+        this.data = data;
+    }           
+    
+     public Node(){
+    }           
+        
+    
+//    setters and getters for node object/ class
+    public PriorityItem getData() {
+        return data;
+    }
+
+    public void setData(PriorityItem data) {
+        this.data = data;
+    }
+
+    public Node getNext() {
+        return next;
+    }
+
+    public void setNext(Node next) {
+        this.next = next;
+    }
+    
+}
